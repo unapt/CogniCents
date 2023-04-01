73066dd5c5c89117fe67cb0d0bc38cd8f91fa39b
joe <joseph.white.bsa@gmail.com>
2023-04-01 09:47:57 -0400

    made sidebar

---
+++ packages/frontend/src/components/SideBar.tsx
@@ -0,0 +1,164 @@
+import React, { ReactNode } from 'react';
+import {
+    IconButton,
+    Box,
+    CloseButton,
+    Flex,
+    Icon,
+    useColorModeValue,
+    Link,
+    Drawer,
+    DrawerContent,
+    Text,
+    useDisclosure,
+    BoxProps,
+    FlexProps,
+    Highlight,
+} from '@chakra-ui/react';
+import {
+    FiHome,
+    FiMenu,
+    FiActivity,
+    FiTarget,
+    FiBookOpen,
+    FiLifeBuoy
+} from 'react-icons/fi';
+import { IconType } from 'react-icons';
+import { ReactText } from 'react';
+
+interface LinkItemProps {
+    name: string;
+    icon: IconType;
+    link: string;
+}
+const LinkItems: Array<LinkItemProps> = [
+    { name: 'Home', icon: FiHome, link: '/' },
+    { name: 'Literacy', icon: FiBookOpen, link: '/literacy' },
+    { name: 'Assistive Automation', icon: FiTarget, link: '/assistive' },
+    { name: 'Personalized Advise', icon: FiLifeBuoy, link: '/personalizedadvise' },
+];
+
+export function SideBar({ children }: { children: ReactNode }) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+            <SideBarContent
+                onClose={() => onClose}
+                display={{ base: 'none', md: 'block' }}
+            />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size="full">
+                <DrawerContent>
+                    <SideBarContent onClose={onClose} />
+                </DrawerContent>
+            </Drawer>
+            {/* mobilenav */}
+            <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
+            <Box ml={{ base: 0, md: 60 }} p="4">
+                {children}
+            </Box>
+        </Box>
+    );
+}
+
+interface SideBarProps extends BoxProps {
+    onClose: () => void;
+}
+
+const SideBarContent = ({ onClose, ...rest }: SideBarProps) => {
+    return (
+        <Box
+            bg={useColorModeValue('white', 'gray.900')}
+            borderRight="1px"
+            borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+            w={{ base: 'full', md: 60 }}
+            pos="fixed"
+            h="full"
+            {...rest}>
+            <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+                <Link href="/">
+                    <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+                        CogniCents
+                    </Text>
+                </Link>
+                <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+            </Flex>
+            {LinkItems.map((link) => (
+                <NavItem key={link.name} icon={link.icon} link1={link.link}>
+                    {link.name}
+                </NavItem>
+            ))}
+        </Box>
+    );
+};
+
+interface NavItemProps extends FlexProps {
+    icon: IconType;
+    link1: string;
+    children: ReactText;
+}
+const NavItem = ({ link1, icon, children, ...rest }: NavItemProps) => {
+    return (
+        <Link href={link1} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+            <Flex
+                align="center"
+                p="4"
+                mx="4"
+                borderRadius="lg"
+                role="group"
+                cursor="pointer"
+                _hover={{
+                    bg: 'cyan.400',
+                    color: 'white',
+                }}
+                {...rest}>
+                {icon && (
+                    <Icon
+                        mr="4"
+                        fontSize="16"
+                        _groupHover={{
+                            color: 'white',
+                        }}
+                        as={icon}
+                    />
+                )}
+                {children}
+            </Flex>
+        </Link>
+    );
+};
+
+interface MobileProps extends FlexProps {
+    onOpen: () => void;
+}
+const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
+    return (
+        <Flex
+            ml={{ base: 0, md: 60 }}
+            px={{ base: 4, md: 24 }}
+            height="20"
+            alignItems="center"
+            bg={useColorModeValue('white', 'gray.900')}
+            borderBottomWidth="1px"
+            borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+            justifyContent="flex-start"
+            {...rest}>
+            <IconButton
+                variant="outline"
+                onClick={onOpen}
+                aria-label="open menu"
+                icon={<FiMenu />}
+            />
+
+            <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
+                Logo
+            </Text>
+        </Flex>
+    );
+};
