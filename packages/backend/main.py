57153c71db4883a9db057165cecf88b1e1d36b76
joe <joseph.white.bsa@gmail.com>
2023-04-01 09:49:11 -0400

    summarizer tool

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from transformers import pipeline
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#LITERACY FUNCTIONS
+
+@app.get("/summerize")
+async def summerize(data: Data):
+    """
+    returns a summerization of message that is provided
+    """
+    
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+    return summarizer(data.message, max_length=200, min_length=50, do_sample=False)
+
+
+
+#ASSISTIVE FUNCTIONS
+
+
+
+
+
+
+#PERSONALIZED FUNCTIONS
+
