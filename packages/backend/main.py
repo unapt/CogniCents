9588434ad383b384d2cb571451e09d3058132653
Dan Unhuryan <unguryan77@gmail.com>
2023-04-01 18:08:30 -0400

    api test

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 import requests
+import json
 
 app = FastAPI()
 
@@ -26,14 +27,36 @@ async def summerize(data: Data):
     
     return summarizer(data.message)
 
+#CAPITAL ONE TRANSFER FUNCTIONS
 
+access_token = 'efb0257be8149ced78cda81ac90aed35'
+payer_id = '64288d889683f20dd5184181'
+payee_id = '6428897e9683f20dd518417e'
 
-#ASSISTIVE FUNCTIONS
+#test transfer ONLY
+transfer_amount = 50000
 
+api_endpoint = f'http://api.nessieisreal.com/accounts/{payer_id}/transfers?key={access_token}'
 
+# Set the headers for the API request
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json'
+}
 
+# Set the JSON payload for the API request
+payload = {
+    'medium': 'balance',
+    'payee_id': payee_id,
+    'transaction_date': '2023-04-01',
+    'status': 'pending',
+    'description': 'Demo transfer',
+    'amount': transfer_amount
+}
 
+# Send the API request
+response = requests.post(api_endpoint, headers=headers, data=json.dumps(payload))
 
-
-#PERSONALIZED FUNCTIONS
+# Print the response from the API
+print(response.text)
 
