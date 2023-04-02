0ef9a625e02d0c1acd43f01a4c3e1ddc30d4906b
joe <joseph.white.bsa@gmail.com>
2023-04-01 20:35:42 -0400

    more backed functions to handle tenq summarization

--- packages/backend/openaiTesting.py
+++ packages/backend/openaiTesting.py
@@ -3,6 +3,10 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 from sec_api import ExtractorApi
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+import requests
 
 dotenv_path = Path('..\HackPrinceton\.env')
 load_dotenv()
@@ -15,56 +19,64 @@ def tenq():
     # 10-Q example
     url_10q = "https://www.sec.gov/Archives/edgar/data/1318605/000095017022006034/tsla-20220331.htm"
 
-    part2_item_1A_text = extractorApi.get_section(url_10q, "part2item1a", "text")
+    part2_item_1A_text = extractorApi.get_section(
+        url_10q, "part2item1a", "text")
     return part2_item_1A_text
 
-def getSummary(filing):
 
+def summarizePart(partFiling):
     gpt_prompt = f"""Your job is to summerize the following information. 
     The information is a 10q for some company that was filed to the sec. 
     You need to dumb it down and make it easy for the average person to 
     understand. Please indicate the most importand information for investment
-    related concepts. Please summerize this in 3 paragraphs.
+    related concepts. Please summerize this in 3 paragraphs made up of at least
+    three sentences per paragraph.
     """
 
     completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo", 
-    messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-03-02"},
-    {"role": "user", "content" : "How are you?"},
-    {"role": "assistant", "content" : "I am doing well"},
-    {"role": "user", "content" : "What is the mission of the company OpenAI?"}]
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": gpt_prompt},
+                  {"role": "user", "content": partFiling}
+                  ]
     )
-    #print(completion)
-    print(completion)
+
+    return (str(completion['choices'][0]['message']['content']))
 
 
-    # conversation=[
-    #     {"role": "system", "content": gpt_prompt},
-    #     {"role": "user", "content": filing},
-    # ]
-    # conversation=[
-    #     {"role": "system", "content": gpt_prompt}
-    # ]
-    # conversation.append({"role": "user", "content": filing})
-    # response = openai.ChatCompletion.create(
-    #     engine="gpt-3.5-turbo",
-    #     messages=conversation,
-    #     max_tokens=4090,
-    #     temperature=0.5, #A low temperature (near 0) is going to give very well-defined answers consistently while a higher number (near 1) will be more creative in its responses
-    #     top_p=1.0
-    # )
+def getSummary(wholeFiling):
+    filing = wholeFiling
+    totalSummary = ""
 
-    
-    #return(response['choices'][0]['message']['content'])
-    #return(response)
+    while (len(filing) > (4*4090)):
+        info = (filing[:(4*4090)]) if len(filing) > 75 else filing
+        filing = filing[(4090*4):]
+        totalSummary = totalSummary + summarizePart(info)
 
-# filing = tenq()
-# if(len(filing) > (4*4090)):
-#     info = (filing[:(4*4090)]) if len(filing) > 75 else filing
-#     print(getSummary(str(info)))
-# else:
-#     print(getSummary(filing))
+    if (len(filing) > 0):
+        totalSummary = totalSummary + summarizePart(filing)
+    return totalSummary
 
 
+def getArticleTitle(message):
+    gpt_prompt = f"""Your job is to title this article that 
+    is provided to you.
+    """
+    info = message
+    if (len(message) > (4*4090)):
+        info = (message[:(4*4090)])
 
-getSummary()
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": gpt_prompt},
+                  {"role": "user", "content": info}
+                  ]
+    )
+
+    return (str(completion['choices'][0]['message']['content']))
+
+
+def determineCompany(context):
+    question_answerer = pipeline(
+        "question-answering", model='distilbert-base-cased-distilled-squad')
+
+    return (question_answerer(question="What company is this paragraph refering to?", context=context))
