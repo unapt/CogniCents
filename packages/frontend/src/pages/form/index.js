da22ab4f628cfb6ce2677f2c1b8510312a9f0ed3
brettseaton <brettseaton0@gmail.com>
2023-04-01 23:42:05 -0400

    verbwire is good

--- packages/frontend/src/pages/form/index.js
+++ packages/frontend/src/pages/form/index.js
@@ -1,49 +1,203 @@
-import Head from 'next/head'
-import styles from '../../styles/Form.module.css'
-import {useRouter} from 'next/router'
+// import Head from "next/head";
+// import { useRouter } from "next/router";
+// import {
+//   Box,
+//   Button,
+//   Flex,
+//   FormControl,
+//   FormLabel,
+//   Input,
+//   VStack,
+//   Heading,
+// } from "@chakra-ui/react";
+// import { SideBar } from "../../components/SideBar";
+
+// export default function Form() {
+//   const router = useRouter();
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const data = {
+//       housingExpenses: event.target.housingExpenses.value,
+//       healthExpenses: event.target.healthExpenses.value,
+//       entertainmentExpenses: event.target.entertainmentExpenses.value,
+//       savingsExpenses: event.target.savingsExpenses.value,
+//       miscellaneousExpenses: event.target.miscellaneousExpenses.value,
+//     };
+//     router.push({
+//       pathname: "/form/results",
+//       query: { q: JSON.stringify(data) },
+//     });
+//   };
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <SideBar>
+//         <Box as="main" textAlign="center" py={10}>
+//           <Heading as="h1" size="2xl" mb={10}>
+//             Enter your information
+//           </Heading>
+//           <form onSubmit={handleSubmit}>
+//             <VStack spacing={4} maxWidth="400px" margin="0 auto">
+//               <FormControl isRequired>
+//                 <FormLabel>Monthly housing expenses:</FormLabel>
+//                 <Input
+//                   name="housingExpenses"
+//                   type="number"
+//                   placeholder="$ Your monthly housing expenses"
+//                 />
+//               </FormControl>
+
+//               <FormControl isRequired>
+//                 <FormLabel>Monthly health expenses:</FormLabel>
+//                 <Input
+//                   name="healthExpenses"
+//                   type="number"
+//                   placeholder="$ Your monthly health expenses"
+//                 />
+//               </FormControl>
+
+//               <FormControl isRequired>
+//                 <FormLabel>Monthly entertainment expenses:</FormLabel>
+//                 <Input
+//                   name="entertainmentExpenses"
+//                   type="number"
+//                   placeholder="$ Your monthly entertainment expenses"
+//                 />
+//               </FormControl>
+
+//               <FormControl isRequired>
+//                 <FormLabel>Monthly savings:</FormLabel>
+//                 <Input
+//                   name="savingsExpenses"
+//                   type="number"
+//                   placeholder="$ Your monthly savings"
+//                 />
+//               </FormControl>
+
+//               <FormControl isRequired>
+//                 <FormLabel>Monthly miscellaneous expenses:</FormLabel>
+//                 <Input
+//                   name="miscellaneousExpenses"
+//                   type="number"
+//                   placeholder="$ Miscellaneous expenses"
+//                 />
+//               </FormControl>
+
+//               <Button type="submit" colorScheme="teal" width="100%">
+//                 Submit
+//               </Button>
+//             </VStack>
+//           </form>
+//         </Box>
+//       </SideBar>
+//     </>
+//   );
+// }
+
+import Head from "next/head";
+import { useRouter } from "next/router";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+} from "@chakra-ui/react";
+import { SideBar } from "../../components/SideBar";
 
 export default function Form() {
-    const router = useRouter();
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = {
-            housingExpenses: event.target.housingExpenses.value,
-            healthExpenses: event.target.healthExpenses.value,
-            entertainmentExpenses: event.target.entertainmentExpenses.value,
-            savingsExpenses: event.target.savingsExpenses.value,
-            miscellaneousExpenses: event.target.miscellaneousExpenses.value
-        }
-        router.push({pathname: "/form/results", query: {q: JSON.stringify(data)}});
-    }
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      housingExpenses: event.target.housingExpenses.value,
+      healthExpenses: event.target.healthExpenses.value,
+      entertainmentExpenses: event.target.entertainmentExpenses.value,
+      savingsExpenses: event.target.savingsExpenses.value,
+      miscellaneousExpenses: event.target.miscellaneousExpenses.value,
+    };
+    router.push({
+      pathname: "/form/results",
+      query: { q: JSON.stringify(data) },
+    });
+  };
 
   return (
-      <>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
 
-      <main id={styles.main}>
-        <h1 id={styles.mainHeader}>Enter your information</h1>
-        <form onSubmit={handleSubmit} id={styles.form}>
-            
-          <label className={styles.formlabel}>Monthly housing expenses:</label>
-          <input required name="housingExpenses" className="form-control" id={styles.forminput} type="number" placeholder="$ Your monthly housing expenses"/>
-          <label className={styles.formlabel}>Monthly health expenses:</label>
-          <input required name="healthExpenses" className="form-control" id={styles.forminput} type="number" placeholder="$ Your monthly health expenses"/>
-          <label className={styles.formlabel}>Monthly entertainment expenses:</label>
-          <input required name="entertainmentExpenses" className="form-control" id={styles.forminput} type="number" placeholder="$ Your monthly entertainment expenses"/>
-          <label className={styles.formlabel}>Monthly savings:</label>
-          <input required name="savingsExpenses" className="form-control" id={styles.forminput} type="number" placeholder="$ Your monthly savings"/>
-          <label className={styles.formlabel}>Monthly miscellaneous expenses:</label>
-          <input required name="miscellaneousExpenses" className="form-control" id={styles.forminput} type="number" placeholder="$ Miscellaneous expenses"/>
-            <div id={styles.buttonlayout}>
-                <button type="submit" id={styles.submitbutton}>Submit</button>
-            </div>
-          
-        </form>
-      </main>
-      </>
-  )
+      <SideBar bg="teal.500">
+        <Box as="main" textAlign="center" py={10}>
+          <Heading as="h1" size="2xl" mb={10}>
+            Enter your information
+          </Heading>
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4} maxWidth="400px" margin="0 auto">
+              <FormControl isRequired>
+                <FormLabel>Monthly housing expenses:</FormLabel>
+                <Input
+                  name="housingExpenses"
+                  type="number"
+                  placeholder="$ Your monthly housing expenses"
+                />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel>Monthly health expenses:</FormLabel>
+                <Input
+                  name="healthExpenses"
+                  type="number"
+                  placeholder="$ Your monthly health expenses"
+                />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel>Monthly entertainment expenses:</FormLabel>
+                <Input
+                  name="entertainmentExpenses"
+                  type="number"
+                  placeholder="$ Your monthly entertainment expenses"
+                />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel>Monthly savings:</FormLabel>
+                <Input
+                  name="savingsExpenses"
+                  type="number"
+                  placeholder="$ Your monthly savings"
+                />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel>Monthly miscellaneous expenses:</FormLabel>
+                <Input
+                  name="miscellaneousExpenses"
+                  type="number"
+                  placeholder="$ Miscellaneous expenses"
+                />
+              </FormControl>
+
+              <Button type="submit" bg="black" color="white" width="100%">
+                Submit
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </SideBar>
+    </>
+  );
 }
