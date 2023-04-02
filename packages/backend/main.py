88b6e73744b9910c7e9a2e86baa698147d4622af
joe <joseph.white.bsa@gmail.com>
2023-04-02 00:57:52 -0400

    more tool

--- packages/backend/main.py
+++ packages/backend/main.py
@@ -1,10 +1,22 @@
+import openai
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from sec_api import ExtractorApi
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 import requests
-import os
+import json
+
 
 app = FastAPI()
+
+dotenv_path = Path('..\HackPrinceton\.env')
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+secapikey = os.getenv("SEC-API-KEY")
+extractorApi = ExtractorApi(secapikey)
 
 
 class Data(BaseModel):
@@ -15,19 +27,115 @@ class Data(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-# LITERACY FUNCTIONS
-
 
 @app.post("/summerize")
 async def summerize(data: Data):
     """
     returns a summerization of message that is provided
     """
-    print(data.message)
     summarizer = pipeline(
         "summarization", model="philschmid/bart-large-cnn-samsum")
 
     return summarizer(data.message)
+
+
+def summarizePart(partFiling):
+    gpt_prompt = f"""Your job is to summerize the following information. 
+    The information is a 10q for some company that was filed to the sec. 
+    You need to dumb it down and make it easy for the average person to 
+    understand. Please indicate the most importand information for investment
+    related concepts. Please summerize this in 3 paragraphs made up of at least
+    three sentences per paragraph.
+    """
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": gpt_prompt},
+                  {"role": "user", "content": partFiling}
+                  ]
+    )
+
+    return (str(completion['choices'][0]['message']['content']))
+
+
+txt = Path(
+    r'C:\Users\joe55\Desktop\HackPrinceton\packages\backend\dailyArticles\txtFileFive.txt').read_text()
+txt1 = Path(
+    r'C:\Users\joe55\Desktop\HackPrinceton\packages\backend\dailyArticles\txtFileFour.txt').read_text()
+txt2 = Path(
+    r'C:\Users\joe55\Desktop\HackPrinceton\packages\backend\dailyArticles\txtFileThree.txt').read_text()
+txt3 = Path(
+    r'C:\Users\joe55\Desktop\HackPrinceton\packages\backend\dailyArticles\txtFileTwo.txt').read_text()
+txt4 = Path(
+    r'C:\Users\joe55\Desktop\HackPrinceton\packages\backend\dailyArticles\txtFileOne.txt').read_text()
+
+allFiles = [txt, txt1, txt2, txt3, txt4]
+
+
+def getSummary(filing):
+    totalSummary = ""
+    for file in allFiles:
+        while (len(filing) > (4*4090)):
+            info = (filing[:(4*4090)]) if len(filing) > 75 else filing
+            filing = filing[(4090*4):]
+            totalSummary = totalSummary + summarizePart(info)
+
+        if (len(filing) > 0):
+            totalSummary = totalSummary + summarizePart(filing)
+
+    return totalSummary
+
+
+@app.get("/getAllSummarys")
+def getAllSummarys():
+    one = getSummary(allFiles[0])
+    two = getSummary(allFiles[1])
+    three = getSummary(allFiles[2])
+    four = getSummary(allFiles[3])
+    five = getSummary(allFiles[4])
+
+    titleone = getArticleTitle(one)
+    titletwo = getArticleTitle(two)
+    titlethree = getArticleTitle(three)
+    titlefour = getArticleTitle(four)
+    titlefive = getArticleTitle(five)
+
+    compone = determineCompany(one)
+    comptwo = determineCompany(two)
+    compthree = determineCompany(three)
+    compfour = determineCompany(four)
+    compfive = determineCompany(five)
+
+    summary_dict = {"summary": [one, two, three, four, five],
+                    "teaser": [titleone, titletwo, titlethree, titlefour, titlefive],
+                    "company": [compone, comptwo, compthree, compfour, compfive]}
+    summary_json = json.dumps(summary_dict)
+    return summary_json
+
+
+def getArticleTitle(message: Data):
+    gpt_prompt = f"""Your job is to title this article that 
+    is provided to you.
+    """
+    info = message
+    if (len(message) > (4*4090)):
+        info = (message[:(4*4090)])
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": gpt_prompt},
+                  {"role": "user", "content": info}
+                  ]
+    )
+
+    return (str(completion['choices'][0]['message']['content']))
+
+
+def determineCompany(context: Data):
+    question_answerer = pipeline(
+        "question-answering", model='distilbert-base-cased-distilled-squad')
+
+    return (question_answerer(question="What company is this paragraph refering to?", context=context))
 
 
 # ASSISTIVE FUNCTIONS
